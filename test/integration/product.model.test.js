15b3b055723d94c74818cdd50c4122422a4d620d
arscombinatoria <harpoon.1942.mercury+github@gmail.com>
2026-05-22 08:29:44 +0900

    test: use ephemeral ports for integration sails apps

--- test/integration/product.model.test.js
+++ test/integration/product.model.test.js
@@ -10,7 +10,7 @@ beforeAll(async () => {
         hooks: { grunt: false },
         log: { level: 'warn' },
         datastores: { default: { adapter: 'sails-disk', inMemoryOnly: true } },
-        port: 1338,
+        port: 0,
       },
       (e) => (e ? rej(e) : res())
     )
